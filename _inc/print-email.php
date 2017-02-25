c0eea7a2da36212ee99896e7afb2679c528c9a45
Caroline Hagan <info@blueocto.co.uk>
2017-02-25 22:43:44 +0000

    Updated the print/email include

--- _inc/print-email.php
+++ _inc/print-email.php
@@ -1,11 +1,11 @@
-<p><strong>Print options</strong> - What are my print options</a></p>
+<hr />
+<p><strong>Print options</strong> - What are my print options <i class="fa fa-question-circle-o" aria-hidden="true"></i></a></p>
 <ul>
 	<li><a href="#" title="">Add guide to my print portfolio (print later as PDF)</a></li>
 </ul>
-<p><strong>Email options</strong> - What are my email options</a></p>
+<p><strong>Email options</strong> - What are my email options <i class="fa fa-question-circle-o" aria-hidden="true"></i></a></p>
 <ul>
 	<li><a href="#">Tell a friend about this page</a></li>
 	<li><a href="#">Send me an email alert when this guide changes</a></li>
 </ul>
-
-<img src="../../../_img/hr_325px_global.gif" alt="" />
+<hr />
