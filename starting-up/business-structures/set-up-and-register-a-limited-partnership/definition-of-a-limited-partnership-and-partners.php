ed81ea0686359b050310f6ab3e99bc398ca4e3b7
Caroline Hagan <info@blueocto.co.uk>
2017-04-02 19:29:27 +0100

    Copied over completed section from BL and changed references to VL.

---
+++ starting-up/business-structures/set-up-and-register-a-limited-partnership/definition-of-a-limited-partnership-and-partners.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Definition of a limited partnership and partners | Venture Labs</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			<h1>Set up and register a limited partnership</h1>
+			<h2>Definition of a limited partnership and partners</h2>
+			<p>A limited partnership is any partnership that includes one or more general partners and one or more limited partners among its officers. Limited partners differ to general partner by contributing money as either capital or property valued at a specific amount.</p>
+			<p>A limited partnership is a different business model to a limited liability partnership. For more information, see the page on <a href="/starting-up/business-structures/legal-structures-the-basics/partnerships.php">partnerships</a> in our guide on <a href="/starting-up/business-structures/legal-structures-the-basics.php">legal structures: the basics</a>.</p>
+			
+			<h3>Differences between general and limited partners</h3>
+			<p>Any individual or legal body - for example a company - may be a partner in a limited partnership, either as a general or as a limited partner. However, you cannot be a general and a limited partner at the same time.</p>
+			<p>You will have different debts and obligations in a limited partnership, depending on which type of partner you are:</p>
+			<ul>
+				<li>general partners are liable for all debts and obligations of the partnership</li>
+				<li>limited partners are only liable for the debts or obligations they put into the business</li>
+			</ul>
+
+			<p>Because limited partners have some form of legal protection from incurring any additional debts, they may not:</p>
+			<ul>
+				<li>take out their contribution to the partnership for as long as it exists</li>
+				<li>control or manage the business</li>
+				<li>have the power to make any binding decisions for the firm</li>
+			</ul>
+
+			<p>If a limited partner chooses to remove any of their original contribution they will lose their protection, and become liable for debts or obligations up to the amount they have received from the partnership. In addition, if a limited partner opts to manage the business for a length of time, they will become liable for any debts or obligations incurred throughout this period.</p>
+			<p>A limited partnership must be registered with Companies House. For more information, see the page in this guide on <a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership/how-to-register-a-limited-partnership.php">how to register a limited partnership</a>.</p>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership.php" title="Set up and register a limited partnership">Prev</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership/how-to-register-a-limited-partnership.php" title="How to register a limited partnership">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership.php" title="Set up and register a limited partnership">Introduction</a></li>
+				<li>Definition of a limited partnership and partners</li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership/how-to-register-a-limited-partnership.php">How to register a limited partnership</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership/tax-matters-of-a-limited-partnership.php">Tax matters of a limited partnership</a></li>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-limited-partnership/dissolving-a-limited-partnership.php">Dissolving a limited partnership</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<dl>
+				<dt></dt>
+				<dd>
+					<a href="https://www.gov.uk/government/organisations/companies-house" target="_blank" rel="noopener noreferrer"><img src="../../../_img/ch_logo_new_3.jpg" width="130" height="35" border="0" alt="" />Companies House</a>
+				</dd>
+			</dl>
+
+			<h3>Companies House Contact Centre</h3>
+			<p>0303 1234 500</p>
+
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/starting-up/business-structures/set-up-and-register-a-partnership.php" title="The legal steps you have to take to form a partnership, including guidance about deeds of partnership">Set up and register a partnership</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Understand the different business types</a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Choose the right business model and legal structure </a></li>
+				<li><a href="https://www.gov.uk/set-up-business" target="_blank" rel="noopener noreferrer">Name your business </a></li>
+			</ul>
+
+			<img src="../../../_img/px_startingabusiness.gif" alt="" width="181" height="5" />
+
+
+	</div><!-- // .small-12 large-8 large-push-4 columns -->
+
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+
+</div><!-- // .row -->
+
+<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
