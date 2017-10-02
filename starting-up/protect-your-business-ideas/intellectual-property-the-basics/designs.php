ecb4963e91917b71fa91c091af851a3da79f3c81
Caroline Hagan <info@blueocto.co.uk>
2017-10-02 22:06:12 +0100

    Completed "starting-up > protect-your-business-ideas > intellectual-property-the-basics" section.

---
+++ starting-up/protect-your-business-ideas/intellectual-property-the-basics/designs.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html class="no-js">
+
+<head>
+	<title>Designs | Venture Labs</title>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/meta.php'; ?>
+</head>
+
+<body>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/header.php'; ?>
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/breadcrumbs.php'; ?>
+	<div class="row">
+		<div class="small-12 large-8 large-push-4 columns">
+			
+			<h1>Intellectual property: the basics</h1>
+			<h2>Designs</h2>
+			<p>You can protect the shape or arrangement of three-dimensional objects in two ways. You will automatically be granted <strong>design right</strong>, and in the UK this gives you protection from copying or misuse by other people or businesses.</p>
+			<p>You can also <strong>register a design</strong>. This may give stronger protection and allow you to extend your design right to other countries. For more information, see our guide on <a href="/create-innovate-and-protect/protecting-your-intellectual-property/copyright-design-and-protecting-your-ideas/design-right-and-registration.php">design right and registration</a>.</p>
+			<p>You must avoid infringing another owner's design right. If you want to use someone else's design, you may be able to come to an arrangement with the rights owner.</p>
+			<p>You cannot register two-dimensional designs such as textile patterns, wallpaper designs, or graphic design and they do not have automatic design right. If you want to protect them, you should use copyright. See the page in this guide on <a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/copyright.php" title="Guidance on what copyright is and how it is granted">copyright</a>.</p>
+
+			<ul>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/trade-marks.php" title="Trade marks">Prev</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/a-start-up-intellectual-property-checklist.php" title="A start-up intellectual property checklist">Next</a></li>
+			</ul>
+			
+			<h3>Subjects covered in this guide</h3>
+			<ul>
+				<li><a href="/create-innovate-and-protect/protecting-your-intellectual-property/patent-protection-and-your-business/intellectual-property-the-basics.php" title="Intellectual property: the basics">Introduction</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/what-is-intellectual-property.php">What is intellectual property?</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/how-you-can-protect-your-intellectual-property.php">How you can protect your intellectual property</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/patents.php">Patents</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/copyright.php">Copyright</a></li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/trade-marks.php">Trade marks</a></li>
+				<li>Designs</li>
+				<li><a href="/starting-up/protect-your-business-ideas/intellectual-property-the-basics/a-start-up-intellectual-property-checklist.php">A start-up intellectual property checklist</a></li>
+			</ul>
+			
+			<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/print-email.php'; ?>
+
+			<h3>Actions</h3>
+			<ul>
+				<li><a href="https://www.gov.uk/government/organisations/intellectual-property-office" target="_blank" rel="noopener noreferrer">Intellectual property information on the Intellectual Property Office website</a></li>
+			</ul>
+			
+			<h3>Also on this site</h3>
+			<ul>
+				<li><a href="/create-innovate-and-protect/protecting-your-intellectual-property/copyright-design-and-protecting-your-ideas/design-right-and-registration.php">Design right and registration</a></li>
+				<li><a href="http://webarchive.nationalarchives.gov.uk/20120729181630/http://online.businesslink.gov.uk/bdotg/action/hubChassis?itemId=1097101996&r.i=1087060130&r.l1=1073858805&r.l2=1073859173&r.l3=1087059738&r.t=RESOURCES&type=HUBCHASSIS" target="_blank" rel="noopener noreferrer">Protect your business and premises</a></li>
+			</ul>
+
+			<p>Developed with:<br />
+				<a href="https://www.gov.uk/government/organisations/intellectual-property-office" target="_blank" rel="noopener noreferrer">
+					<img src="/_img/IPO_LOGO.gif" width="150" height="26"  alt="" /> Intellectual Property Office
+				</a>
+			</p>
+
+			
+		</div>
+		<!-- // .small-12 large-8 large-push-4 columns -->
+		<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/category-links.php'; ?>
+	</div>
+	<!-- // .row -->
+	<?php include $_SERVER['DOCUMENT_ROOT'].'/_inc/footer.php'; ?>
