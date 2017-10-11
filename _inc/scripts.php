0a4225ad9ed4cdb5bb3c8df4fe25442c1caf7c81
Caroline Hagan <info@blueocto.co.uk>
2017-10-11 22:23:51 +0100

    Ran Gulp for the scripts to update. Downgraded jQuery, removed specific domain as not required.

--- _inc/scripts.php
+++ _inc/scripts.php
@@ -1,11 +1,11 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
 <?php /* Load What-Input files in footer */ ?>
 <!-- <script defer src="http://alpha.venturelabs.org.uk/_vendor/what-input/dist/what-input.min.js"></script> -->
 <?php /* Adding Foundation scripts file in the footer */ ?>
-<script defer src="http://alpha.venturelabs.org.uk/assets/js/foundation.js"></script>
+<script defer src="/assets/js/foundation.js"></script>
 <?php /* Load What-Input files in footer */ ?>
-<script defer src="http://alpha.venturelabs.org.uk/assets/js/scripts.js"></script>
+<script defer src="/assets/js/scripts.js"></script>
 <?php /* Typekit - Proxima Nova Soft
 <script defer src="https://use.typekit.net/hnm1usr.js"></script>
 <script>try{Typekit.load({ async: true });}catch(e){}</script>
