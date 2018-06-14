f3f2c10e27c885affb5a6f2e9f0d0712cbcb6077
Caroline Hagan <cmurphydesign@gmail.com>
2018-06-15 00:03:17 +0100

    Removed un-used code, updated jQuery version, removed the need for Font Awesome; using local SVG. Updated styles to reflect.

--- _inc/scripts.php
+++ _inc/scripts.php
@@ -1,16 +1,9 @@
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
-<?php /* Load What-Input files in footer */ ?>
-<!-- <script defer src="http://alpha.venturelabs.org.uk/_vendor/what-input/dist/what-input.min.js"></script> -->
 <?php /* Adding Foundation scripts file in the footer */ ?>
 <script defer src="/assets/js/foundation.min.js"></script>
 <?php /* Load What-Input files in footer */ ?>
 <script defer src="/assets/js/scripts.min.js"></script>
-<?php /* Typekit - Proxima Nova Soft
-<script defer src="https://use.typekit.net/hnm1usr.js"></script>
-<script>try{Typekit.load({ async: true });}catch(e){}</script>
- */ ?>
-<?php /* Font Awesome CDN */ ?>
-<script src="https://use.fontawesome.com/4f840bacf9.js"></script>
+
 
 <script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)})(window,document,'script','https://www.google-analytics.com/analytics.js','ga');ga('create', 'UA-92653253-1', 'auto');ga('send', 'pageview');</script>
